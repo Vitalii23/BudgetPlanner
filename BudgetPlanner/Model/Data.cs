540b71b6c9e1f71641c5a174bb408e3a14e22371
Vitalii Danilov <Vitalii Danilov@DESKTOP-GIENFQV>
2022-05-14 20:01:09 +0700

    edit: Data.cs

--- BudgetPlanner/Model/Data.cs
+++ BudgetPlanner/Model/Data.cs
@@ -9,6 +9,13 @@ namespace BudgetPlanner.Model
     class Data
     {
         public ushort Id { get; set; }
-        public string ElementIncome { get; set }
+        public string ElementIncome { get; set; }
+        public string ElementExpenses { get; set; }
+        public int InCash { get; set; }
+        public int OutCash { get; set; }
+        public DateTime DateAddCash { get; set; }
+        public DateTime DateRemoveCash { get; set; }
+        public int ResultIncome { get; set; }
+        public int ResultExpenses { get; set; }
     }
 }
