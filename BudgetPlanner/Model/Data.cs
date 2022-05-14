366bc5fdc3db44574eeaf72b6e2a9d788a16837d
Vitalii Danilov <Vitalii Danilov@DESKTOP-GIENFQV>
2022-05-14 19:19:56 +0700

    Добавьте файлы проекта.

---
+++ BudgetPlanner/Model/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Model
+{
+    class Data
+    {
+        public ushort Id { get; set; }
+        public string ElementIncome { get; set }
+    }
+}
